60f8f6391dcc1bf7bbd06f21c8dc4759e030f0d3
Stahl4121 <Stahl4121@users.noreply.github.com>
2019-11-19 00:09:14 -0500

    Routing works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.css';
+import Home from './Home.js';
+import Schedule from './Schedule.js';
+import DJShows from './DJShows.js';
+import Contact from './Contact.js';
+import { Link, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-       <ul>
-       <li>Home</li>
-       <li>DJ Shows</li>
-       <li>Schedule</li>
-       <li>Contact Info</li>
-       </ul>
-       {this.props.children}
-    </div>
- )
+   <div>
+      <ul>
+         <li><Link to="/">Home</Link></li>
+         <li><Link to="/shows">DJ Shows</Link></li>
+         <li><Link to="/schedule">Schedule</Link></li>
+         <li><Link to="/contact">Contact Info</Link></li>
+      </ul>
+
+      <Route exact={true} path="/" component={Home}/>
+      <Route exact={true} path="/shows" component={DJShows}/>
+      <Route exact={true} path="/schedule" component={Schedule}/>
+      <Route exact={true} path="/contact" component={Contact}/>
+   </div>
+  )
 }
 export default App;
