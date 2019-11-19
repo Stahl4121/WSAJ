7652c1258b28f03e4d6ddb6c2fa61d747a818978
Stahl4121 <Stahl4121@users.noreply.github.com>
2019-11-19 00:18:24 -0500

    reorganized navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import './App.css';
-import Home from './Home.js';
-import Schedule from './Schedule.js';
-import DJShows from './DJShows.js';
-import Contact from './Contact.js';
-import { Link, Route } from 'react-router-dom';
+import Navbar from './Navbar.js';
 
 function App() {
   return (
-   <div>
-      <ul>
-         <li><Link to="/">Home</Link></li>
-         <li><Link to="/shows">DJ Shows</Link></li>
-         <li><Link to="/schedule">Schedule</Link></li>
-         <li><Link to="/contact">Contact Info</Link></li>
-      </ul>
-
-      <Route exact={true} path="/" component={Home}/>
-      <Route exact={true} path="/shows" component={DJShows}/>
-      <Route exact={true} path="/schedule" component={Schedule}/>
-      <Route exact={true} path="/contact" component={Contact}/>
-   </div>
+   <Navbar/>
   )
 }
 export default App;
