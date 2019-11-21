52677c5d811d8bdcd48fe9ac0e3f2802c003d663
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-11-20 23:40:22 -0500

    added basics

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: theme.spacing(6, 0, 3),
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+  return (
+    <Typography className={classes.?} color=?>
+  
+    </Typography>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import theme from './theme';
 import 'typeface-roboto';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import * as serviceWorker from './comp/serviceWorker';
 
-ReactDOM.render(<BrowserRouter> <App /> </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>,
+    document.querySelector('#root'),
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
