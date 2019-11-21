b9fb4c2646953a9e1ad72adaaf333d1f325bad60
sarahcalvis <sarahcalvis@gmail.com>
2019-11-21 15:00:36 -0500

    horizontal nav boyyyyyyyyyyyyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,16 +8,29 @@ const useStyles = makeStyles(theme => ({
   root: {
     margin: theme.spacing(6, 0, 3),
   },
+  appBar: {
+    listStyleType: "none",
+
+    backgroundColor: "#333",
+    top: "0",
+    width: "100%",
+    display: "inline",
+
+  },
+  tab: {
+    color: "#F7FAFF",
+    textAlign: "center",
+  },
 }));
 
 export default function NavBar() {
   const classes = useStyles();
   return (
-    <AppBar title="WSAJ">
-        <Tab label="Home" />
-        <Tab label="DJ Shows" />
-        <Tab label="Calendar" />
-        <Tab label="Contact" />
+    <AppBar position="static" className={classes.appBar} title="WSAJ">
+        <Tab className={classes.tab} label="Home" />
+        <Tab className={classes.tab} label="DJ Shows" />
+        <Tab className={classes.tab} label="Calendar" />
+        <Tab className={classes.tab} label="Contact" />
     </AppBar>
   );
 }
