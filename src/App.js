547a8264df3754aef8c1031f7b17a43fefbac6b1
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-11-22 10:41:46 -0500

    old calendar junk removed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import './App.css';
 import NavBar from './NavBar.js';
 import DJCard from './DJCard';
 import FullWidthGrid from './CardGrid';
-import DisplayDailyCalendar from './DisplayDailyCalendar';
 function App() {
   return (
     <div>
       <NavBar/>
       <FullWidthGrid/>
       <FullWidthGrid/>
-      <DisplayDailyCalendar/>
     </div> 
   )
 }
