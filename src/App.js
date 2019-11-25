c8a689e07c57c0c6a8398c849d0199e375677d3f
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-11-25 17:58:14 -0500

    work on broken calendar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'typeface-roboto';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import FullWidthGrid from './components/CardGrid';
-import { Calendar, Views } from 'react-big-calendar'
 
 function App() {
   return (
