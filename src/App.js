b90fa1ac12a4928ff5b801a8c32026263d2dcaa4
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-11-25 09:40:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import 'typeface-roboto';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import FullWidthGrid from './components/CardGrid';
+import { Calendar, Views } from 'react-big-calendar'
+
 function App() {
   return (
     <div>
       <NavBar/>
       <FullWidthGrid/>
       <FullWidthGrid/>
+      <Calendar/>
     </div> 
   )
 }
