3fb37392cb1535509cfac427bd8209904edb7578
Stahl4121 <Stahl4121@users.noreply.github.com>
2019-11-27 17:02:17 -0500

    CArD gRiD dEd

--- src/screens/DJShowsScreen.js
+++ src/screens/DJShowsScreen.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import FullWidthGrid from '../components/CardGrid';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import DJCard from '../components/DJCard';
+import DJShows from '../data/DJShows.json';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+}));
 
 export default function DJShowsScreen() {
-    return <FullWidthGrid/>;
+    const classes = useStyles();
+    var shows = DJShows["Shows"];
+    var cards = [];
+
+    console.log(shows);
+
+    for (var i = 0; i < shows.length; i++) {
+        // note: we add a key prop here to allow react to uniquely identify each
+        // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+        cards.push(<Grid container item xs={3} spacing={0}><DJCard show={shows[i]} /></Grid>);
+    }
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={0}>{cards}</Grid>
+        </div>
+    );
 }
