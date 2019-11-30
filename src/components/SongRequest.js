d925678519023dbfcd7bbc68681e288e40727206
sarahcalvis <sarahcalvis@gmail.com>
2019-11-29 21:11:28 -0500

    some song request improvements good night folks

--- src/components/SongRequest.js
+++ src/components/SongRequest.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 const styles = theme => ({
   icon: {
-      marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2),
   },
   heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
   },
   heroButtons: {
-      marginTop: theme.spacing(4),
+    marginTop: theme.spacing(4),
   },
   cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
   },
   card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
   },
   cardMedia: {
-      paddingTop: '56.25%', // 16:9
-      height: 250,
+    paddingTop: '56.25%', // 16:9
+    height: 250,
   },
   cardContent: {
-      flexGrow: 1,
+    flexGrow: 1,
   },
   footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
   },
 });
 class SongRequest extends React.Component {
@@ -51,21 +52,30 @@ class SongRequest extends React.Component {
   };
   render() {
     const { classes } = this.props;
-    return(
+    return (
+
       <div className={classes.heroContent}>
-      <Container maxWidth="sm">
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-          Request a Song
-        </Typography>
-        <TextField
-          align="center"
-          justify="center"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          label="Song Request"
-          onChange={this.autofill}
-        />
+        <Container maxWidth="sm">
+          <div className={classes.heroButtons}>
+            <Grid container spacing={2} /*justify="center"*/
+  direction="row"
+  justify="space-around"
+  alignItems="center">
+              <Grid item xs={10}>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  label="Request a song"
+                  onChange={this.autofill}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Button variant="contained" color="primary" fullWidth>
+                  Submit
+                  </Button>
+              </Grid>
+            </Grid>
+          </div>
         </Container>
       </div>
     );
