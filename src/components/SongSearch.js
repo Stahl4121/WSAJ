341148563638118eb9143cea87f2e79dd367fa0c
sarahcalvis <sarahcalvis@gmail.com>
2019-12-03 00:00:29 -0500

    it WORKKKKKKKKSSSSSSSSSSSSSSSSSSSSSSSSSs (kinda)

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -4,45 +4,32 @@ import deburr from 'lodash/deburr';
 import Downshift from 'downshift';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import $ from 'jquery';
 import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
-
-const suggestions = [
-  { label: 'Afghanistan' },
-  { label: 'Aland Islands' },
-  { label: 'Albania' },
-  { label: 'Algeria' },
-  { label: 'American Samoa' },
-  { label: 'Andorra' },
-  { label: 'Angola' },
-  { label: 'Anguilla' },
-  { label: 'Antarctica' },
-  { label: 'Antigua and Barbuda' },
-  { label: 'Argentina' },
-  { label: 'Armenia' },
-  { label: 'Aruba' },
-  { label: 'Australia' },
-  { label: 'Austria' },
-  { label: 'Azerbaijan' },
-  { label: 'Bahamas' },
-  { label: 'Bahrain' },
-  { label: 'Bangladesh' },
-  { label: 'Barbados' },
-  { label: 'Belarus' },
-  { label: 'Belgium' },
-  { label: 'Belize' },
-  { label: 'Benin' },
-  { label: 'Bermuda' },
-  { label: 'Bhutan' },
-  { label: 'Bolivia, Plurinational State of' },
-  { label: 'Bonaire, Sint Eustatius and Saba' },
-  { label: 'Bosnia and Herzegovina' },
-  { label: 'Botswana' },
-  { label: 'Bouvet Island' },
-  { label: 'Brazil' },
-  { label: 'British Indian Ocean Territory' },
-  { label: 'Brunei Darussalam' },
-];
+import Chip from '@material-ui/core/Chip';
+var suggestions = [];
+function loadSuggestions(query) {
+    var settings = {
+    "async": false,
+    "crossDomain": true,
+    "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + query,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+      "x-rapidapi-key": "f33e47e69fmshe427476175d1511p18d30djsn436f42242136"
+    }
+  }
+  var suggestions = [];
+  $.ajax(settings).done(function (response) {
+    if(typeof response.data !== 'undefined') {
+      for (var i = 0; i < response.data.length; i ++) {
+        suggestions.push({label: response.data[i].title + ' by ' + response.data[i].artist.name});
+      }
+    }
+  });
+  return suggestions;
+}
 
 function renderInput(inputProps) {
   const { InputProps, classes, ref, ...other } = inputProps;
@@ -71,8 +58,9 @@ renderInput.propTypes = {
 };
 
 function renderSuggestion(suggestionProps) {
-  const { suggestion, index, itemProps, highlightedIndex} = suggestionProps;
+  const { suggestion, index, itemProps, highlightedIndex, selectedItem } = suggestionProps;
   const isHighlighted = highlightedIndex === index;
+  const isSelected = (selectedItem || '').indexOf(suggestion.label) > -1;
 
   return (
     <MenuItem
@@ -81,7 +69,7 @@ function renderSuggestion(suggestionProps) {
       selected={isHighlighted}
       component="div"
       style={{
-        //fontWeight: isSelected ? 500 : 400,
+        fontWeight: isSelected ? 500 : 400,
       }}
     >
       {suggestion.label}
@@ -89,24 +77,137 @@ function renderSuggestion(suggestionProps) {
   );
 }
 
+renderSuggestion.propTypes = {
+  highlightedIndex: PropTypes.oneOfType([PropTypes.oneOf([null]), PropTypes.number]).isRequired,
+  index: PropTypes.number.isRequired,
+  itemProps: PropTypes.object.isRequired,
+  selectedItem: PropTypes.string.isRequired,
+  suggestion: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 function getSuggestions(value, { showEmpty = false } = {}) {
   const inputValue = deburr(value.trim()).toLowerCase();
   const inputLength = inputValue.length;
   let count = 0;
-
+  suggestions = loadSuggestions(inputValue);
   return inputLength === 0 && !showEmpty
     ? []
     : suggestions.filter(suggestion => {
-        const keep =
-          count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+      const keep =
+        count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
 
-        if (keep) {
-          count += 1;
-        }
+      if (keep) {
+        count += 1;
+      }
 
-        return keep;
-      });
+      return keep;
+    });
 }
+
+function DownshiftMultiple(props) {
+  const { classes } = props;
+  const [inputValue, setInputValue] = React.useState('');
+  const [selectedItem, setSelectedItem] = React.useState([]);
+
+  const handleKeyDown = event => {
+    if (selectedItem.length && !inputValue.length && event.key === 'Backspace') {
+      setSelectedItem(selectedItem.slice(0, selectedItem.length - 1));
+    }
+  };
+
+  const handleInputChange = event => {
+    loadSuggestions(event.target.value);
+    setInputValue(event.target.value);
+  };
+
+  const handleChange = item => {
+    let newSelectedItem = [...selectedItem];
+    if (newSelectedItem.indexOf(item) === -1) {
+      newSelectedItem = [...newSelectedItem, item];
+    }
+    setInputValue('');
+    setSelectedItem(newSelectedItem);
+  };
+
+  const handleDelete = item => () => {
+    const newSelectedItem = [...selectedItem];
+    newSelectedItem.splice(newSelectedItem.indexOf(item), 1);
+    setSelectedItem(newSelectedItem);
+  };
+
+  return (
+    <Downshift
+      id="downshift-multiple"
+      inputValue={inputValue}
+      onChange={handleChange}
+      selectedItem={selectedItem}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getLabelProps,
+        isOpen,
+        inputValue: inputValue2,
+        selectedItem: selectedItem2,
+        highlightedIndex,
+      }) => {
+        const { onBlur, onChange, onFocus, ...inputProps } = getInputProps({
+          onKeyDown: handleKeyDown,
+          placeholder: 'Select multiple countries',
+        });
+
+        return (
+          <div className={classes.container}>
+            {renderInput({
+              fullWidth: true,
+              classes,
+              label: 'Countries',
+              InputLabelProps: getLabelProps(),
+              InputProps: {
+                startAdornment: selectedItem.map(item => (
+                  <Chip
+                    key={item}
+                    tabIndex={-1}
+                    label={item}
+                    className={classes.chip}
+                    onDelete={handleDelete(item)}
+                  />
+                )),
+                onBlur,
+                onChange: event => {
+                  handleInputChange(event);
+                  onChange(event);
+                },
+                onFocus,
+              },
+              inputProps,
+            })}
+
+            {isOpen ? (
+              <Paper className={classes.paper} square>
+                {getSuggestions(inputValue2).map((suggestion, index) =>
+                  renderSuggestion({
+                    suggestion,
+                    index,
+                    itemProps: getItemProps({ item: suggestion.label }),
+                    highlightedIndex,
+                    selectedItem: selectedItem2,
+                  }),
+                )}
+              </Paper>
+            ) : null}
+          </div>
+        );
+      }}
+    </Downshift>
+  );
+}
+
+DownshiftMultiple.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -134,9 +235,14 @@ const useStyles = makeStyles(theme => ({
     width: 'auto',
     flexGrow: 1,
   },
+  divider: {
+    height: theme.spacing(2),
+  },
 }));
 
-export default function IntegrationDownshift() {
+let popperNode;
+
+export default function SongSearch() {
   const classes = useStyles();
 
   return (
@@ -145,23 +251,24 @@ export default function IntegrationDownshift() {
         {({
           getInputProps,
           getItemProps,
+          getLabelProps,
           getMenuProps,
           highlightedIndex,
           inputValue,
           isOpen,
+          selectedItem,
         }) => {
           const { onBlur, onFocus, ...inputProps } = getInputProps({
-            placeholder: 'Request a song',
+            placeholder: 'Request a song you want to hear on this show',
           });
 
           return (
             <div className={classes.container}>
               {renderInput({
                 fullWidth: true,
-                variant:"outlined",
-                label: "Request a song",
-                margin:"normal",
                 classes,
+                label: 'Song Request',
+                InputLabelProps: getLabelProps({ shrink: true }),
                 InputProps: { onBlur, onFocus },
                 inputProps,
               })}
@@ -175,6 +282,7 @@ export default function IntegrationDownshift() {
                         index,
                         itemProps: getItemProps({ item: suggestion.label }),
                         highlightedIndex,
+                        selectedItem,
                       }),
                     )}
                   </Paper>
