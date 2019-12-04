90475870ddf5f0ee4ffc4926e8a0cff1e7483b9b
sarahcalvis <sarahcalvis@gmail.com>
2019-12-03 23:14:57 -0500

    i doubt this works

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -2,20 +2,48 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-
-export default function ComboBox() {
-  return (
-    <Autocomplete
-      id="combo-box-demo"
-      options={top100Films}
-      getOptionLabel={option => option.title}
-      style={{ width: 300 }}
-      renderInput={params => (
-        <TextField {...params} label="Combo box" variant="outlined" fullWidth />
-      )}
-    />
-  );
-}
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+import $ from 'jquery';
 var songs = [];
+
+class SongSearch extends React.Component {
+  handleOnChange = event => {
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + event.target.value,
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "f33e47e69fmshe427476175d1511p18d30djsn436f42242136"
+      }
+    }
+    var suggestions = [];
+    $.ajax(settings).done(function (response) {
+      if (typeof response.data !== 'undefined') {
+        var newSongs = [];
+        for (var i = 0; i < response.data.length; i++) {
+          newSongs.push({ label: response.data[i].title + ' by ' + response.data[i].artist.name });
+        }
+        songs = newSongs;
+      }
+    });
+  };
+  render() {
+    return (
+      <Autocomplete
+        id="combo-box-demo"
+        options={songs}
+        getOptionLabel={option => option.title}
+        //style={{ width: 300 }}
+        renderInput={params => (
+          <TextField {...params}
+            label="Song Request"
+            variant="outlined"
+            fullWidth
+            onKeyPress={this.handleOnChange}/>
+        )}
+      />
+    );
+  }
+}
+export default (SongSearch);
