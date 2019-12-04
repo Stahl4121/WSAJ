34d85535804b0d724fea1acbf931f500d14b61ca
sarahcalvis <sarahcalvis@gmail.com>
2019-12-04 15:20:29 -0500

    class layout but there are async problems

--- src/screens/DJShowsScreen.js
+++ src/screens/DJShowsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import DJCard from '../components/DJCard';
 import DJShows from '../data/DJShows.json';
@@ -8,7 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import firebase from "../firebase.js"
 
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
     icon: {
         marginRight: theme.spacing(2),
     },
@@ -38,42 +38,54 @@ const useStyles = makeStyles(theme => ({
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(6),
     },
-}));
+});
 
-export default function DJShowsScreen() {
-    const classes = useStyles();
-    var cards = [];
-    var db = firebase.firestore();
+class DJShowsScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            fields: {},
+            errors: {}
+        }
+        this.queryDB = this.queryDB.bind(this);
+    };
 
-    db.collection("shows").get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            var name = doc.data().name
-            console.log(name);
-            // doc.data() is never undefined for query doc snapshots
-            //console.log(doc.id, " => ", doc.data().genre + ' ' + doc.data().description + ' ' + doc.data().name);
-            cards.push(<Grid item /*key={card}*/ xs={12} sm={6} md={4}><DJCard show={name} /></Grid>);
+    queryDB() {
+        var cards = [];
+        var db = firebase.firestore();
+
+        db.collection("shows").get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                var name = doc.data().name
+                cards.push(<Grid item xs={12} sm={6} md={4}><DJCard show={name} /></Grid>);
+            });
         });
-    });
-    return (
-        <div>
-            {/* Hero unit */}
-            <div className={classes.heroContent}>
-                <Container maxWidth="sm">
-                    <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                        WSAJ Shows
+        return cards;
+    };
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                {/* Hero unit */}
+                <div className={classes.heroContent}>
+                    <Container maxWidth="sm">
+                        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                            WSAJ Shows
                     </Typography>
-                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                        We copied this site:{"\n"}
-                        https://material-ui.com/getting-started/templates/album/{"\n"}
-                        almost exactly to create this cardview. Click a show to check it out!
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            We copied this site:{"\n"}
+                            https://material-ui.com/getting-started/templates/album/{"\n"}
+                            almost exactly to create this cardview. Click a show to check it out!
                     </Typography>
+                    </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4} >
+                        {this.queryDB()}
+                    </Grid>
                 </Container>
             </div>
-            <Container className={classes.cardGrid} maxWidth="md">
-                <Grid container spacing={4} >
-                    {cards}
-                </Grid>
-            </Container>
-        </div>
-    );
+        );
+    }
 }
+export default withStyles(styles)(DJShowsScreen);
