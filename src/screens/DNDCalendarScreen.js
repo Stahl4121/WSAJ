3150f116dd7a2c6018319bdd80d1e84695c68145
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-12-09 11:52:18 -0500

    hack fix to drap and drop bug

--- src/screens/DNDCalendarScreen.js
+++ src/screens/DNDCalendarScreen.js
@@ -18,123 +18,28 @@ class DNDCalendarScreen extends React.Component {
       errors: {},
       events: [
         {
-          start: new Date(),
-          end: new Date(moment().add(1, "days")),
-          title: "Some title"
-        },
-        {
           id: 0,
-          title: 'All Day Event very long title',
-          allDay: true,
-          start: new Date(2019, 11, 0),
-          end: new Date(2019, 11, 1),
+          start: new Date(),
+          end: new Date(moment().add(1, "h")),
+          title: "First Event"
         },
         {
-            id: 1,
-            title: 'Long Event',
-            start: new Date(2019, 11, 7),
-            end: new Date(2019, 11, 10),
-        },
-    
-        {
-            id: 2,
-            title: 'DTS STARTS',
-            start: new Date(2016, 2, 13, 0, 0, 0),
-            end: new Date(2016, 2, 20, 0, 0, 0),
-        },
-    
-        {
-            id: 3,
-            title: 'DTS ENDS',
-            start: new Date(2016, 10, 6, 0, 0, 0),
-            end: new Date(2016, 10, 13, 0, 0, 0),
-        },
-    
-        {
-            id: 4,
-            title: 'Some Event',
-            start: new Date(2019, 11, 9, 0, 0, 0),
-            end: new Date(2019, 11, 10, 0, 0, 0),
+          id: 1,
+          start: new Date(),
+          end: new Date(moment().add(1, "h")),
+          title: "Ice Cream Time"
         },
         {
-            id: 5,
-            title: 'Conference',
-            start: new Date(2019, 11, 11),
-            end: new Date(2019, 11, 13),
-            desc: 'Big conference for important people',
-        },
-        {
-            id: 6,
-            title: 'Meeting',
-            start: new Date(2019, 11, 12, 10, 30, 0, 0),
-            end: new Date(2019, 11, 12, 12, 30, 0, 0),
-            desc: 'Pre-meeting meeting, to prepare for the meeting',
-        },
-        {
-            id: 7,
-            title: 'Lunch',
-            start: new Date(2019, 11, 12, 12, 0, 0, 0),
-            end: new Date(2019, 11, 12, 13, 0, 0, 0),
-            desc: 'Power lunch',
-        },
-        {
-            id: 8,
-            title: 'Meeting',
-            start: new Date(2019, 11, 12, 14, 0, 0, 0),
-            end: new Date(2019, 11, 12, 15, 0, 0, 0),
-        },
-        {
-            id: 9,
-            title: 'Happy Hour',
-            start: new Date(2019, 11, 12, 17, 0, 0, 0),
-            end: new Date(2019, 11, 12, 17, 30, 0, 0),
-            desc: 'Most important meal of the day',
-        },
-        {
-            id: 12,
-            title: 'Late Night Event',
-            start: new Date(2019, 11, 17, 19, 30, 0),
-            end: new Date(2019, 11, 18, 2, 0, 0),
-        },
-        {
-            id: 12.5,
-            title: 'Late Same Night Event',
-            start: new Date(2019, 11, 17, 19, 30, 0),
-            end: new Date(2019, 11, 17, 23, 30, 0),
-        },
-        {
-            id: 13,
-            title: 'Multi-day Event',
-            start: new Date(2019, 11, 20, 19, 30, 0),
-            end: new Date(2019, 11, 22, 2, 0, 0),
-        },
-        {
-            id: 14,
-            title: 'Today',
-            start: new Date(new Date().setHours(new Date().getHours() - 3)),
-            end: new Date(new Date().setHours(new Date().getHours() + 3)),
+          id: 2,
+          start: new Date(),
+          end: new Date(moment().add(1, "h")),
+          title: "Snack Time"
         },
       ],
     }
     this.moveEvent = this.moveEvent.bind(this);
     //this.newEvent = this.newEvent.bind(this);
   };
-
-  // onEventResize = (type, { event, start, end, allDay }) => {
-  //   this.setState(state => {
-  //     state.events[0].start = start;
-  //     state.events[0].end = end;
-  //     console.log(start);
-  //     console.log(end);
-  //     console.log(event);
-  //     console.log(allDay);
-  //     return { events: state.events };
-  //   });
-  // };
-
-  // onEventDrop = ({ event, start, end, allDay }) => {
-  //   console.log(start);
-  // };
 
   moveEvent({ event, start, end, isAllDay: droppedOnAllDaySlot }) {
     const { events } = this.state
