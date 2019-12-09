7999cf3e503af7a7fb91a9ed96657645f796aba2
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-12-09 11:13:35 -0500

    drag and drop starting to work!!!!

---
+++ src/screens/DNDCalendarScreen.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
+
+
+class DNDCalendarScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fields: {},
+      errors: {},
+      events: [
+        {
+          start: new Date(),
+          end: new Date(moment().add(1, "days")),
+          title: "Some title"
+        }
+      ],
+    }
+  };
+
+  onEventResize = (type, { event, start, end, allDay }) => {
+    this.setState(state => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: state.events };
+    });
+  };
+
+  onEventDrop = ({ event, start, end, allDay }) => {
+    console.log(start);
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className="dnd">
+        <DnDCalendar
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          localizer={localizer}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default (DNDCalendarScreen);
