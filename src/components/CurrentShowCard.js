1ffcdaec8ad794e47411e5e3a8e95185d1c0660e
Nathaniel Sprecher <nathanielsprecher@5cg731061y.gcc.edu>
2019-12-09 23:58:58 -0500

    Actually compiles now

--- src/components/CurrentShowCard.js
+++ src/components/CurrentShowCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import firebase from "../firebase.js";
+
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import TextField from "@material-ui/core/TextField";
-import firebase from "../firebase.js";
 import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
